d2ad1fd0c85a1deb6916c5bc0d86290a5878a4f3
Jorge Rocha <jorge.rocha.blanco@gmail.com>
2026-02-20 03:09:20 +0100

    .

--- app_st.py
+++ app_st.py
@@ -49,7 +49,7 @@ def response_generator():
 
 # Display assistant response in chat message container
 with st.chat_message("assistant"):
-    response = agent.chat(st.session_state.messages["content"])
+    response = agent.chat(prompt)
 # Add assistant response to chat history
 st.session_state.messages.append({"role": "assistant", "content": response})
 
