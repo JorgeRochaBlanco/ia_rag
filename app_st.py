84bef0415e15ec9b5840001b3a373dd22c32010e
Jorge Rocha <jorge.rocha.blanco@gmail.com>
2026-02-20 02:48:25 +0100

    Generador automático de respuestas

--- app_st.py
+++ app_st.py
@@ -8,10 +8,14 @@ from app.agent import FAQAgent
 load_dotenv()
 
 # Initializa agent
-agent = FAQAgent()
+#agent = FAQAgent()
 
-# Title
-st.title("ChatGPT-like clone")
+
+import streamlit as st
+import random
+import time
+
+st.title("Simple chat")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -19,20 +23,39 @@ if "messages" not in st.session_state:
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message):
-        st.markdown(message)
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 # Accept user input
-if prompt := st.chat_input("Qué quieres saber?"):
-    # Add user message to chat history
-    st.session_state.messages.append(prompt)
+if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
 
 # Display assistant response in chat message container
 with st.chat_message("assistant"):
-    # Get response from agent
-    response = agent.chat(message.content)
+    response = st.write_stream(response_generator())
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
 
-st.session_state.messages.append(response)
+
+
+
+# Get response from agent
+#response = agent.chat(message.content)
